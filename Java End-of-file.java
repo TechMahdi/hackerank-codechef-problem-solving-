d7e5232122de33027016fbff90d4301a09fb5fe0
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-30 00:00:46 +0600

    Create Java End-of-file.java

---
+++ Java End-of-file.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int n=1;
+        String line;
+        while(input.hasNext())
+        {
+            line =input.nextLine();
+            System.out.println(n+" "+line);
+            n++;
+            
+            
+        }
+    }
+}
