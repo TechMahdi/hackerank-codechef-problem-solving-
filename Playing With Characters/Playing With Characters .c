ef9af7985508519e6a61b0f22a540c6e038c03b6
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-10-09 16:40:12 +0600

    Create Playing With Characters .c

---
+++ Playing With Characters/Playing With Characters .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    char ch ;
+    char s[100];
+    char s1[100];
+    scanf("%c",&ch);
+    scanf("\n");
+    gets(s);
+    gets(s1);
+    printf("%c\n",ch);
+puts(s);
+puts(s1);
+}
